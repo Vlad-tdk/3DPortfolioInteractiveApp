62bc6dc7286c3b99530dc6a1767d6ae64c083395
Vlad <xalif80@gmail.com>
2025-06-05 20:03:27 +0200

    Initial Commit

---
+++ 3DInteractiveApp/_DInteractiveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DInteractiveAppApp.swift
+//  3DInteractiveApp
+//
+//  Created by Vladimir Martemianov on 5. 6. 2025..
+//
+
+import SwiftUI
+
+@main
+struct _DInteractiveAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
